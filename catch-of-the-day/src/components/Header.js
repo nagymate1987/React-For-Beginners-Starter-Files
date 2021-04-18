7920e12f136ca4157fe1597b313cbe7532c7dfe7
nagymate1987 <nagymate1987@gmail.com>
2021-04-18 20:45:24 +0200

    Add files via upload

--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -1,6 +1,24 @@
 import React from "react";
 
-class Header extends React.Component {
+const Header = ( props ) => (
+
+		<header className="top">
+					<h1>
+						Catch 
+						<span className="ofThe">
+							<span className="of">of</span>
+							<span className="the">the</span>
+						</span>
+						Day
+					</h1>
+					<h3 className="tagline">
+					<span>{props.tagline} </span>
+				</h3>
+				</header>
+);
+
+
+/*class Header extends React.Component {
 	render() {
 		return (
 			<React.Fragment>
@@ -14,13 +32,13 @@ class Header extends React.Component {
 						Day
 					</h1>
 					<h3 className="tagline">
-					<span>Fresh Daily</span>
+					<span>{this.props.tagline}</span>
 				</h3>
 				</header>
 				
 			</React.Fragment>
 		)
 	}
-}
+} */
 
 export default Header;
